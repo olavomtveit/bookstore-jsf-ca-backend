b415762d6c2702314c2dd4923929d98ae259f824
olavomtveit <omtveit94@gmail.com>
2021-03-28 14:57:33 +0200

    new strapi

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '62fdbc4da500f0c1260159db2ee7fe6c'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '22a029b1-ee00-47da-be8b-92c562e31ab3'
+};
